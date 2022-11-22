b8d5226ff6e7271be1e2c964bee0cc860ccffd75
AlanFNL <89108362+AlanFNL@users.noreply.github.com>
2022-11-22 11:10:11 -0300

    Added web3

--- src/New/New.js
+++ src/New/New.js
@@ -10,12 +10,13 @@ import C2 from '../assets/cloud2.png'
 import C3 from '../assets/cloud3.png'
 import C4 from '../assets/cloud4.png'
 import C5 from '../assets/cloud5.png'
-import A from '../assets/avi.png'
+import B from '../assets/fanfest2.gif'
+import confetti from '../assets/confetti.gif'
 
 
 function New() {
 
-    const TestAddress = "0x9a5A110c9CA8aBaC2731F5F27D1e539b304fdCE5";
+    const TestAddress = "0x0fE085946253f069ec1f22A9867740c8e7C292f2";
     const [mintAmount, setMintAmount] = useState(1);
     const [whiteInfo, setWhiteInfo] = useState({
         address: "-",
@@ -58,6 +59,44 @@ function New() {
     }
 }
 
+const handleWhitelis = async (e) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const amount = 1;
+    const mint = 0.02
+    const signer = await provider.getSigner();
+    const signerAddress = await signer.getAddress();
+    const contract = new ethers.Contract(
+        TestAddress,
+        Test.abi,
+        signer
+    );
+    const tx = await contract.mint(mint) 
+    try { 
+        const receipt = await tx.wait()
+        console.log(receipt)
+        
+        }
+     catch (err) {
+        alert(err)
+        
+
+    }
+}
+
+
+
+let popup = document.querySelector('.popup_mint');
+let close = document.querySelector('.close_mint');
+
+const Ref_m = useRef()
+
+    
+    const BotonOff = () => {
+        Ref_m.current.classList.remove('active')
+    
+    }
+
 async function handleMint() {
     if (window.ethereum) {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -67,11 +106,15 @@ async function handleMint() {
             Test.abi,
             signer
         );
-        try {
-            const response = await contract.mint(BigNumber.from(mintAmount), {
-                value: ethers.utils.parseEther((0 * mintAmount).toString()),
+        
+        const response = await contract.mint(BigNumber.from(mintAmount), {
+                value: ethers.utils.parseEther((0.002 * mintAmount).toString()),
             });
-            console.log('response: ', response);
+        try {
+            const receipt = await response.wait()
+            console.log(receipt)
+            popup.classList.add('active')
+            
 
         } catch (err) {
             console.log("error: ", err)
@@ -117,8 +160,8 @@ var x = setInterval(function(){
        <div className='contents flow'>
         <div className='even-columns'>
             <div className='col'>
-                <div className='container_itworks'>
-                <img className='plane' src={A}></img>
+                <div className='container_itworks2'>
+                <img className='plane' src={B}></img>
                 </div>
                 </div>
             <div className='col'>
@@ -149,8 +192,14 @@ var x = setInterval(function(){
         </div>
 
        </div>
+       <div ref={Ref_m} className='popup_mint'>
+        <h2>Congratulations!</h2>
+        <p>Your Collectible has been minted successfully!</p>
+        <img className='confetti' src={confetti}></img>
+        <b onClick={BotonOff} className='close_mint'>x</b>
+       </div>
 
-
+       
         </div>
         
 
